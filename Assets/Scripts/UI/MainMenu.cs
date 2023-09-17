e1874147d9d018a987bb24a4c2234662e5e30af0
Igo0Go <chudakin98@gmail.com>
2023-09-17 23:32:21 +0700

    Сцена главного меню с поддержкой геймпада

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private List<Button> mainButtons;
+    [SerializeField]
+    private List<Button> toolTipButtons;
+    [SerializeField]
+    private GameObject toolTipPanel;
+    [SerializeField]
+    private int playScene = 1;
+
+    void Awake()
+    {
+        mainButtons[0].Select();
+        toolTipPanel.SetActive(false);
+    }
+
+    public void Play() => SceneManager.LoadScene(playScene);
+
+    public void SetActiveForTipPanel(bool value)
+    {
+        toolTipPanel.SetActive(value);
+
+        if (value )
+        {
+            toolTipButtons[0].Select();
+        }
+        else
+        {
+            mainButtons[0].Select();
+        }
+    }
+
+    public void Exit() => Application.Quit();
+
+    public void SetToolTipType(int type)
+    {
+
+    }
+}
